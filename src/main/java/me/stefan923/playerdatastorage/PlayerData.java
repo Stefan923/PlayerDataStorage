dd871c54949c4a69f7c8086fb4646b74fc0afaf4
Stefan923 <stefanpopescu923@gmail.com>
2021-03-14 00:46:32 +0200

    Added a new field (uuid)

--- src/main/java/me/stefan923/playerdatastorage/PlayerData.java
+++ src/main/java/me/stefan923/playerdatastorage/PlayerData.java
@@ -3,18 +3,36 @@ package me.stefan923.playerdatastorage;
 import org.bukkit.Effect;
 import org.bukkit.inventory.ItemStack;
 
-public class PlayerData {
+import java.io.Serializable;
+import java.util.UUID;
+
+public class PlayerData implements Serializable {
 
     private final ItemStack[] inventoryContent;
     private final ItemStack[] enderChestContent;
     private final Effect[] effects;
     private final float experience;
 
+    private transient UUID uuid;
+
     public PlayerData(ItemStack[] inventoryContent, ItemStack[] enderChestContent, Effect[] effects, float experience) {
         this.inventoryContent = inventoryContent;
         this.enderChestContent = enderChestContent;
         this.effects = effects;
         this.experience = experience;
+    }
+
+    public PlayerData(UUID uuid, ItemStack[] inventoryContent, ItemStack[] enderChestContent, Effect[] effects, float experience) {
+        this(inventoryContent, enderChestContent, effects, experience);
+        this.uuid = uuid;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
     }
 
     public ItemStack[] getInventoryContent() {
