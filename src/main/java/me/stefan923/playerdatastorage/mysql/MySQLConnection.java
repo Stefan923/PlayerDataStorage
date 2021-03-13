ea7adf38510e6916338de850e2f473a9ddd2102d
Stefan923 <stefanpopescu923@gmail.com>
2021-03-13 12:39:21 +0200

    Added MySQLConnection class

---
+++ src/main/java/me/stefan923/playerdatastorage/mysql/MySQLConnection.java
@@ -0,0 +1,43 @@
+package me.stefan923.playerdatastorage.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+
+    private final String username;
+    private final String password;
+    private final String tablePrefix;
+    private final String url;
+
+    private Connection connection;
+
+    public MySQLConnection(String tablePrefix, String host, int port, String databaseName, String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.tablePrefix = tablePrefix;
+        this.url = "jdbc:mysql://" + host + ":" + port + "/" + databaseName;
+    }
+
+    public String getTablePrefix() {
+        return tablePrefix;
+    }
+
+    public Connection getConnection() throws SQLException {
+        if (this.connection == null || !this.connection.isValid(5)) {
+            this.connect();
+        }
+
+        return this.connection;
+    }
+
+    private void connect() {
+        try {
+            connection = DriverManager.getConnection(this.url, this.username, this.password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
