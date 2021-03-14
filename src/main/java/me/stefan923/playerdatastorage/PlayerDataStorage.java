edc3515a14f42d827149c132faf276a2f025ac44
Stefan923 <stefanpopescu923@gmail.com>
2021-03-14 12:50:07 +0200

    Added javadocs comments

--- src/main/java/me/stefan923/playerdatastorage/PlayerDataStorage.java
+++ src/main/java/me/stefan923/playerdatastorage/PlayerDataStorage.java
@@ -4,10 +4,26 @@ import java.util.UUID;
 
 public interface PlayerDataStorage {
 
+    /**
+     * Creates database table if it doesn't exist.
+     */
     void createTable();
 
+    /**
+     * Returns player data after loading it from the database.
+     *
+     * @param uuid - player's uuid
+     * @return {@link PlayerData} if given player exists into
+     *                            the database. Otherwise it
+     *                            returns null.
+     */
     PlayerData getPlayerData(UUID uuid);
 
+    /**
+     * Saves the player data in the database.
+     *
+     * @param playerData - player's data to be saved
+     */
     void savePlayerData(PlayerData playerData);
 
 }
