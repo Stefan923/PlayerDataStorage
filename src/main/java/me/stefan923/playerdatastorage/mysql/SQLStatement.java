723f3160691a0d7448a30a23fb63e98e38338f36
Stefan923 <stefanpopescu923@gmail.com>
2021-03-14 18:19:35 +0200

    Updated CREATE_TABLE statement

--- src/main/java/me/stefan923/playerdatastorage/mysql/SQLStatement.java
+++ src/main/java/me/stefan923/playerdatastorage/mysql/SQLStatement.java
@@ -2,7 +2,7 @@ package me.stefan923.playerdatastorage.mysql;
 
 public final class SQLStatement {
 
-    public static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS `{prefix}playerdata` (`uuid` VARCHAR(36), `playerData` BLOB);";
+    public static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS `{prefix}playerdata` (`uuid` VARCHAR(36), `playerData` BLOB, PRIMARY KEY(`uuid`));";
     public static final String SAVE_PLAYER_DATA = "INSERT INTO `{prefix}playerdata` (`uuid`, `playerData`) VALUES(?, ?) ON DUPLICATE KEY UPDATE `playerData` = ?;";
     public static final String GET_PLAYER_DATA = "SELECT `playerData` FROM `{prefix}playerdata` WHERE `uuid` LIKE ?;";
 
